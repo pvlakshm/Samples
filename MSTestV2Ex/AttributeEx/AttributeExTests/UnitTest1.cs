3fdba6ee206bc9c410040fc96cb4f9098b3ed654
Pratap Lakshman <pratapl@microsoft.com>
2017-06-30 17:43:30 +0530

    Added tests using GB18030

--- MSTestV2Ex/AttributeEx/AttributeExTests/UnitTest1.cs
+++ MSTestV2Ex/AttributeEx/AttributeExTests/UnitTest1.cs
@@ -10,21 +10,43 @@ namespace AttributeExTests
         [TestMethod]
         [SmokeTest]
         [Note("No external dependencies!")]
-        public void TestMethod1()
-        {
-        }
+        public void TestMethod1() { }
 
         [TestMethod]
         [Note("CPU intensive")]
-        public void TestMethod2()
-        {
-        }
+        public void TestMethod2() { }
 
         [TestMethod]
         [Note("CPU intensive")]
         [Note("Memory intensive")]
-        public void TestMethod3()
-        {
-        }
+        public void TestMethod3() { }
+
+        // double byte:     { "啊齄", "\u554a\u9f44" },
+        [TestMethod]
+        [Note("啊齄")]
+        public void TestMethod4() { }
+
+        [TestMethod]
+        [Note("\u554a\u9f44")]
+        public void TestMethod4a() { }
+
+        // Four byte: Ext-A { "㐀㒣", "\u3400\u34a3" },
+        [TestMethod]
+        [Note("㐀㒣")]
+        public void TestMethod5() { }
+
+        [TestMethod]
+        [Note("\u3400\u34a3")]
+        public void TestMethod5a() { }
+
+
+        // Four byte: Ext-B { "𠀀𠀁","\ud840\udc00\ud840\udc01"},
+        [TestMethod]
+        [Note("𠀀𠀁")]
+        public void TestMethod6() { }
+
+        [TestMethod]
+        [Note("\ud840\udc00\ud840\udc01")]
+        public void TestMethod6a() { }
     }
 }
