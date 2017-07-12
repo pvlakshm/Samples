3161d5c3bddfd3fc58df0f6827a0b6397a4f93f7
Pratap Lakshman <pratapl@microsoft.com>
2017-07-12 18:51:51 +0530

    Added empty test files.

---
+++ Shapes/ShapesTests/CircleTests.cpp
@@ -0,0 +1,30 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ShapesTests
+{
+    TEST_CLASS(CircleTests)
+    {
+    public:
+        TEST_METHOD(CircleWith0RadiusHas0Area)
+        {
+            // TODO: Your test code here
+        }
+
+        TEST_METHOD(CircleWithNegativeRadiusNHas0Area)
+        {
+            // TODO: Your test code here
+        }
+
+        TEST_METHOD(CircleWithRadius5HasArea78)
+        {
+            // TODO: Your test code here
+        }
+
+        TEST_METHOD(CircleWithRadius100HasArea31415)
+        {
+            // TODO: Your test code here
+        }
+    };
+}
