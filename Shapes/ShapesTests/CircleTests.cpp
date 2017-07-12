d9c3d67950c6055656e0bf58ba7591ebb38c3796
Pratap Lakshman <pratapl@microsoft.com>
2017-07-12 20:19:02 +0530

    Added a few tests for Circle.

--- Shapes/ShapesTests/CircleTests.cpp
+++ Shapes/ShapesTests/CircleTests.cpp
@@ -16,7 +16,7 @@ namespace ShapesTests
             Assert::AreEqual(0, area, DBL_EPSILON);
         }
 
-        TEST_METHOD(CircleWithNegativeRadiusNHas0Area)
+        TEST_METHOD(CircleWithNegativeRadiusHas0Area)
         {
             Circle c = Circle::Circle(-1);
             double area = c.getArea();
@@ -35,6 +35,45 @@ namespace ShapesTests
             Circle c = Circle::Circle(100);
             double area = c.getArea();
             Assert::AreEqual(31415.9, area, 0.1);
+        }
+
+        TEST_METHOD(CircleWithRadius1HasArea3point1415)
+        {
+            Circle c = Circle::Circle(1);
+            double area = c.getArea();
+            Assert::AreEqual(3.14159, area, 0.1);
+        }
+
+        TEST_METHOD(GetArea1000TimesGivesSameValue)
+        {
+            Circle c = Circle::Circle(100);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                double area1 = c.getArea();
+                double area2 = c.getArea();
+
+                Assert::AreEqual(area1, area2, DBL_EPSILON);
+            }
+        }
+
+        TEST_METHOD(GetAreaDoesNotChangeRadius)
+        {
+            Circle c = Circle::Circle(100);
+
+            double area1 = c.getArea();
+            int rad = c.getRadius();
+
+            Assert::AreEqual(100, rad);
+        }
+
+        TEST_METHOD(DefaultCicrleIsNotGreenInColor)
+        {
+            Circle c = Circle::Circle(0);
+
+            string color = c.getColor();
+
+            Assert::IsFalse(color.compare("green") == 0);
         }
     };
 }
