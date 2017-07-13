50a66f9dc205c417cffaea7a6c3e1c551683ffcb
Pratap Lakshman <pratapl@microsoft.com>
2017-07-13 16:58:23 +0530

    Modified cicrle instantiation.

--- Shapes/ShapesTests/CircleTests.cpp
+++ Shapes/ShapesTests/CircleTests.cpp
@@ -18,35 +18,35 @@ namespace ShapesTests
 
         TEST_METHOD(CircleWithNegativeRadiusHas0Area)
         {
-            Circle c = Circle::Circle(-1.0);
+            Circle c(-1.0);
             double area = c.getArea();
             Assert::AreEqual(0.0, area, DBL_EPSILON);
         }
 
         TEST_METHOD(CircleWithRadius5HasArea78)
         {
-            Circle c = Circle::Circle(5.0);
+            Circle c(5.0);
             double area = c.getArea();
             Assert::AreEqual(78.5398, area, 0.1);
         }
 
         TEST_METHOD(CircleWithRadius100HasArea31415)
         {
-            Circle c = Circle::Circle(100.0);
+            Circle c(100.0);
             double area = c.getArea();
             Assert::AreEqual(31415.9, area, 0.1);
         }
 
         TEST_METHOD(CircleWithRadius1HasArea3point1415)
         {
-            Circle c = Circle::Circle(1.0);
+            Circle c(1.0);
             double area = c.getArea();
             Assert::AreEqual(3.14159, area, 0.1);
         }
 
         TEST_METHOD(CircleGetAreaCalled1000TimesGivesSameValue)
         {
-            Circle c = Circle::Circle(100.0);
+            Circle c(100.0);
 
             for (int i = 0; i < 1000; i++)
             {
@@ -59,7 +59,7 @@ namespace ShapesTests
 
         TEST_METHOD(CircleGetAreaDoesNotChangeRadius)
         {
-            Circle c = Circle::Circle(100.0);
+            Circle c(100.0);
 
             double area1 = c.getArea();
             double rad = c.getRadius();
@@ -69,7 +69,7 @@ namespace ShapesTests
 
         TEST_METHOD(CircleDefaultColorIsGreen)
         {
-            Circle c = Circle::Circle(0);
+            Circle c(0);
 
             string color = c.getColor();
 
