4340165c5e87a2e4050ddfbdfbb85b1237b7c08b
Pratap Lakshman <pratapl@microsoft.com>
2017-10-16 13:10:30 +0530

    Cleanups

---
+++ NUnitTests/NUnit2x/NUnit2x/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit2x
+{
+    public class Class1
+    {
+    }
+}
