dcf6a84f08c9da54e6bbb5b86e272e0ac3eb80bd
Pratap Lakshman <pratapl@microsoft.com>
2017-10-17 12:34:57 +0530

    Added code and some tests.

--- NUnitTests/NUnit3x/NUnit3x/Class1.cs
+++ NUnitTests/NUnit3x/NUnit3x/Class1.cs
@@ -8,5 +8,12 @@ namespace NUnit3x
 {
     public class Class1
     {
+        public bool isEven(int i)
+        {
+            if (i % 2 == 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
---
+++ NUnitTests/NUnit3x/NUnit3xTests/Class1Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit3x;
+
+namespace NUnit3xTests
+{
+    public class Class1Tests
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            bool res = new Class1().isEven(4);
+            Assert.IsTrue(res);
+        }
+
+
+        [TestCase]
+        [Category("BVT")]
+        public void TestMethod2()
+        {
+            bool res = new Class1().isEven(5);
+            Assert.IsFalse(res);
+        }
+
+
+        [TestCase(100, true)]
+        [TestCase(101, false)]
+        public void TestMethod3(int val, bool res)
+        {
+            bool b = new Class1().isEven(val);
+            Assert.AreEqual(res, b);
+        }
+    }
+}
