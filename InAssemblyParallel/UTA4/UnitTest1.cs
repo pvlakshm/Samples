35f0cc009b23ff9383bd88ac1c615ff93108c423
Pratap Lakshman <pratapl@microsoft.com>
2017-12-12 17:58:29 +0530

    Added Init/Cleanup methods on all the testlclasses.

--- InAssemblyParallel/UTA4/UnitTest1.cs
+++ InAssemblyParallel/UTA4/UnitTest1.cs
@@ -7,6 +7,63 @@ namespace UTA4
     [TestClass]
     public class UTA4_C1
     {
+        // --------------------------------------------
+        // regardless of parallelization mode, classinit/cleanup should be called only once.
+        // Furthermore, classinit should have been called before classcleanup.
+        // we use a static boolean to check that sequence of calls, and that it was called only once.
+        static bool bTestClassInitCalled = false;
+
+        [ClassInitialize]
+        public static void testclassinit(TestContext tc)
+        {
+            if (bTestClassInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestClassInitCalled = true;
+        }
+
+        [ClassCleanup]
+        public static void testclasscleanup()
+        {
+            // regardless of the parallelization mode, this should be called only once
+            if (!bTestClassInitCalled)
+            {
+                throw new Exception();
+            }
+        }
+        // --------------------------------------------
+
+
+        // --------------------------------------------
+        // regardless of parallelization mode, testinit/cleanup should be called before/after every test in this class.
+        // Furthermore, testinit should have been called before testcleanup.
+        // we use a boolean to check that sequence of calls, and that it was called only once.
+        bool bTestInitCalled = false;
+        [TestInitialize]
+        public void testinit()
+        {
+            if (bTestInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestInitCalled = true;
+        }
+
+        [TestCleanup]
+        public void testleanup()
+        {
+            if (!bTestInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestInitCalled = false; // get it ready fr the next call to testInit()
+        }
+        // --------------------------------------------
+
         [TestMethod]
         public void UTA4_C1_TM1()
         {
@@ -39,6 +96,63 @@ namespace UTA4
     [TestClass]
     public class UTA4_C2
     {
+        // --------------------------------------------
+        // regardless of parallelization mode, classinit/cleanup should be called only once.
+        // Furthermore, classinit should have been called before classcleanup.
+        // we use a static boolean to check that sequence of calls, and that it was called only once.
+        static bool bTestClassInitCalled = false;
+
+        [ClassInitialize]
+        public static void testclassinit(TestContext tc)
+        {
+            if (bTestClassInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestClassInitCalled = true;
+        }
+
+        [ClassCleanup]
+        public static void testclasscleanup()
+        {
+            // regardless of the parallelization mode, this should be called only once
+            if (!bTestClassInitCalled)
+            {
+                throw new Exception();
+            }
+        }
+        // --------------------------------------------
+
+
+        // --------------------------------------------
+        // regardless of parallelization mode, testinit/cleanup should be called before/after every test in this class.
+        // Furthermore, testinit should have been called before testcleanup.
+        // we use a boolean to check that sequence of calls, and that it was called only once.
+        bool bTestInitCalled = false;
+        [TestInitialize]
+        public void testinit()
+        {
+            if (bTestInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestInitCalled = true;
+        }
+
+        [TestCleanup]
+        public void testleanup()
+        {
+            if (!bTestInitCalled)
+            {
+                throw new Exception();
+            }
+
+            bTestInitCalled = false; // get it ready fr the next call to testInit()
+        }
+        // --------------------------------------------
+
         [TestMethod]
         public void UTA4_C2_TM1()
         {
