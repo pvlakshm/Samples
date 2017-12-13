f39a4b74ee121a990176e7529042760bb764c4d7
Pratap Lakshman <pratapl@microsoft.com>
2017-12-13 15:13:20 +0530

    Added Parallize attribute.

--- NUnitTests/NUnit3x/NUnit3xTests/Class1Tests.cs
+++ NUnitTests/NUnit3x/NUnit3xTests/Class1Tests.cs
@@ -6,9 +6,42 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NUnit3x;
 
+
+[assembly: Parallelizable(ParallelScope.All)]
+
+
 namespace NUnit3xTests
 {
     public class Class1Tests
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            bool res = new Class1().isEven(4);
+            Assert.IsTrue(res);
+        }
+
+
+        [TestCase]
+        [Category("BVT")]
+        public void TestMethod2()
+        {
+            bool res = new Class1().isEven(5);
+            Assert.IsFalse(res);
+        }
+
+
+        [TestCase(100, true)]
+        [TestCase(101, false)]
+        public void TestMethod3(int val, bool res)
+        {
+            bool b = new Class1().isEven(val);
+            Assert.AreEqual(res, b);
+        }
+    }
+
+
+    public class Class2Tests
     {
         [TestCase]
         public void TestMethod1()
