235287baf1e5c1783cf6718a4676e3e173503f2c
Pratap Lakshman <pratapl@microsoft.com>
2018-01-19 15:40:47 +0530

    Added comments.

--- MSTestV2Ex/AttributeEx/AttributeEx/NoteAttribute.cs
+++ MSTestV2Ex/AttributeEx/AttributeEx/NoteAttribute.cs
@@ -3,6 +3,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AttributeEx
 {
+    // -------------------------------------------------------------------------------------------
+    // A strongly typped "Note" attribute that allows attaching a string as a note to a testmethod
+    // E.g.
+    // [TestMethod]
+    // [Note("lorem ipsum")]
+    // public void myTestCase() { }
+    // -------------------------------------------------------------------------------------------
+
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class NoteAttribute : TestPropertyAttribute
     {
--- MSTestV2Ex/AttributeEx/AttributeEx/SmokeTestAttribute.cs
+++ MSTestV2Ex/AttributeEx/AttributeEx/SmokeTestAttribute.cs
@@ -3,6 +3,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AttributeEx
 {
+    // -------------------------------------------------------------------------------------------
+    // A strongly typed attribute that allows categorizing a testmethod as a so called SmokeTest.
+    // E.g.
+    // [TestMethod]
+    // [SmokeTest]
+    // public void myTestCase() { }
+    // -------------------------------------------------------------------------------------------
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class SmokeTestAttribute : TestPropertyAttribute
     {
