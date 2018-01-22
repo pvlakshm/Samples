a4b3855e24c1fb6cc919e250cd6ff0c44841acfa
Pratap Lakshman <pratapl@microsoft.com>
2018-01-22 18:29:28 +0530

    NUnit Tests

---
+++ NUnit.Tests5/NUnit.Tests5/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests5
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            //Assert.Pass("Your first passing test");
+            Warn.If(true, "this is a warning");
+            Assert.Inconclusive();
+        }
+    }
+}
