059d87424354f5a5aa3fa33aa9c7925366ac7631
Pratap Lakshman <pratapl@microsoft.com>
2018-02-01 14:57:13 +0530

    New files.

---
+++ NUnitTests/NUnit2x/NUnit2x/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit2x
+{
+    public class Class2
+    {
+        public int sum(int i, int j)
+        {
+            //if (i == 0)
+            //    return j;
+            //else if (j == 0)
+            //    return i;
+            //else
+                return i + j;
+        }
+    }
+}
---
+++ NUnitTests/NUnit2x/NUnit2xTests/Class2Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit2x;
+
+namespace NUnit2xTests
+{
+    class Class2Tests
+    {
+        [TestCase]
+        public void TestSum1()
+        {
+            int res = new Class2().sum(5, 6);
+            Assert.AreEqual(11, res);
+        }
+    }
+}
