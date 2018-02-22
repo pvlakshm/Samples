bec9d4b48fd8b553551e1dc759d5feea3d022bdc
Pratap Lakshman <pratapl@microsoft.com>
2018-02-22 10:36:43 +0530

    Win10 IoT Core Sample

---
+++ Win10_IoT_Core/LED_Test/UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Win10_IoT_Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static LED led = new Win10_IoT_Core.LED();
+
+        [UITestMethod]
+        public void TestMethodON()
+        {
+            // whatever be the current state, ON the LED
+            led.turnON();
+
+            // get the state of the LED pin, and assert on it.
+            var bON = led.State();
+
+            Assert.IsTrue(bON);
+        }
+
+        [UITestMethod]
+        public void TestMethodOFF()
+        {
+            // whatever be the current state, OFF the LED
+            led.turnOFF();
+
+            // get the state of the LED pin, and assert on it.
+            var bON = led.State();
+
+            Assert.IsFalse(bON);
+        }
+    }
+}
---
+++ Win10_IoT_Core/Win10_IoT_Core/LED.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace Win10_IoT_Core
+{
+    public class LED
+    {
+        private GpioPin pin;
+
+        public LED()
+        {
+            InitGPIO();
+        }
+
+        private void delay()
+        {
+            Task.Delay(1000).Wait();
+        }
+
+        private bool InitGPIO()
+        {
+            const int LED_PIN = 5;
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                pin = null;
+                return false;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            return true;
+        }
+
+        public bool turnON()
+        {
+            pin.Write(GpioPinValue.Low);
+            return State();
+        }
+
+        public bool turnOFF()
+        {
+            pin.Write(GpioPinValue.High);
+            return State();
+        }
+
+        public bool State()
+        {
+            delay();
+
+            var val = pin.Read();
+
+            if (val == GpioPinValue.Low)
+                return true;
+            else
+                return false;
+        }
+    }
+}
