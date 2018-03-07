a71416434cee05f18c7d917c9f3bfa8bfdd654cc
Pratap Lakshman <pratapl@microsoft.com>
2018-03-07 15:06:37 +0530

    Math using xUnit tests.

---
+++ Math/Mathlib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathlib
+{
+    public class Class1
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Math/MathlibTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Mathlib;
+
+
+namespace MathlibTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            int x = 5;
+            int y = 6;
+            int sum = 11;
+
+            int res = new Mathlib.Class1().add(x, y);
+            Assert.Equal(sum, res);
+        }
+    }
+}
