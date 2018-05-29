1cff0408ed0808ce14a9e5cc3f8b67e7ee7cc11c
Pratap Lakshman <pratapl@microsoft.com>
2018-05-29 18:14:53 +0530

    added to mult, and div.

---
+++ maven/src/main/java/Divide.java
@@ -0,0 +1,5 @@
+public class Divide {
+    public int Divide_int(int a, int b) {
+        return a / b;
+    }
+}
--- maven/src/main/java/Multiply.java
+++ maven/src/main/java/Multiply.java
@@ -2,4 +2,16 @@ public class Multiply {
     public int Multiply_int(int a, int b) {
         return a * b;
     }
+
+    public int Multiply_intByRepeatedAddition(int a, int b) {
+	    int runningTotal = a;
+		Add a = new Add();
+
+		for (int i = 1; i < b; i++)
+		{
+			runningTotal =+ a.Add_int(runningTotal, a)
+		}
+
+        return runningTotal;
+    }
 }
