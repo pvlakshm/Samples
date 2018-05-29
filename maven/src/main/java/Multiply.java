9d5d91fef2242c896e0a2dd2c314499749c68597
Pratap Lakshman <pratapl@microsoft.com>
2018-05-29 18:26:29 +0530

    fixed error.

--- maven/src/main/java/Multiply.java
+++ maven/src/main/java/Multiply.java
@@ -5,11 +5,11 @@ public class Multiply {
 
     public int Multiply_intByRepeatedAddition(int a, int b) {
 	    int runningTotal = a;
-		Add a = new Add();
+		Add adder = new Add();
 
 		for (int i = 1; i < b; i++)
 		{
-			runningTotal =+ a.Add_int(runningTotal, a);
+			runningTotal += adder.Add_int(runningTotal, a);
 		}
 
         return runningTotal;
