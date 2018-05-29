cc80ad62ee7de29e0b685dafae08a73e6d4a82a1
Pratap Lakshman <pratapl@microsoft.com>
2018-05-29 18:39:53 +0530

    added divide by repeated subtraction.

--- maven/src/main/java/Divide.java
+++ maven/src/main/java/Divide.java
@@ -2,4 +2,19 @@ public class Divide {
     public int Divide_int(int a, int b) {
         return a / b;
     }
+
+    public int Divide_intByRepeatedSubtraction(int a, int b) {
+		int runningTotal = a;
+		Subtract s = new Subtract();
+		int quotient = 0;
+
+		while (runningTotal > 0)
+		{
+			runningTotal = s.Subtract_int(runningTotal, b);
+			quotient++;
+		}
+
+        return quotient;
+    }
+
 }
