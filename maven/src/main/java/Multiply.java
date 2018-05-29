4e9d8367cea81c13ecea45db78e5f0df7be64127
Pratap Lakshman <pratapl@microsoft.com>
2018-05-29 17:17:45 +0530

    added mult, sub.

---
+++ maven/src/main/java/Multiply.java
@@ -0,0 +1,5 @@
+public class Multiply {
+    public int Multiply_int(int a, int b) {
+        return a * b;
+    }
+}
---
+++ maven/src/main/java/Subtract.java
@@ -0,0 +1,5 @@
+public class Subtract {
+    public int Subtract_int(int a, int b) {
+        return a - b;
+    }
+}
