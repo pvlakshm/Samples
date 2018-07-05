d10295314540256a2a4adfa698c47448290e09a9
pvlakshm <pvlakshm@users.noreply.github.com>
2018-07-05 21:03:56 +0530

    added new files. (#29)

---
+++ FlakyTests/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Test
+    {
+        [DataTestMethod]
+        [DataRow("cherry")]
+        [DataRow("banana")]
+        [DataRow("Apple")]
+        public virtual void TestMethod(string a)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestClass]
+    public class MyTest : Test
+    {
+        [DataTestMethod]
+        [DataRow("orange")]
+        [DataRow("pineapple")]
+        public override void TestMethod(string a)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
