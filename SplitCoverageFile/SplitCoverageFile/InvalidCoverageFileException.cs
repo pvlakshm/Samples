e30a0434b2d2a27aea4e1f48b96fd269e60c7634
pvlakshm <pvlakshm@users.noreply.github.com>
2018-07-24 18:54:35 +0530

    Fb1 (#33)
    * added new files.
    
    * added some flaky tests.
    
    * Added a few flaky tests.
    
    * Renamed test for clarity.
    
    * Deleted unused files.
    
    * SplitCoverageFile Utility.
    
    * Fixed references.

---
+++ SplitCoverageFile/SplitCoverageFile/InvalidCoverageFileException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitCoverageFile
+{
+    /// <summary>
+    /// The exception for invalid coverage files.
+    /// </summary>
+    [global::System.Serializable]
+    public class InvalidCoverageFileException : Exception
+    {
+        public InvalidCoverageFileException() { }
+        public InvalidCoverageFileException(string message) : base(message) { }
+        public InvalidCoverageFileException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidCoverageFileException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
