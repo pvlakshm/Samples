23c58b049e73dbdac8fe5d8d4369156d2ce735b9
Pratap Lakshman <pratapl@microsoft.com>
2018-10-19 16:46:11 +0530

    Added TimeoutAttribute sample.

---
+++ TimeoutSample/TimeoutSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TimeoutSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimeoutSample")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75285ebe-330a-4c6c-9563-171ed1195023")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TimeoutSample/TimeoutSample/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimeoutSample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            Thread.Sleep(5000);
+        }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            Thread.Sleep(3000);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            Thread.Sleep(1000);
+        }
+
+        // The TimeoutAttribute considers the total time of a test as made up the
+        // sum of the times of TestInitialize, TestMethod, and TestCleanup.
+        [TestMethod]
+        [Timeout(2100)]    // if you set this to 2 secs or less, the test execution will be timed out.
+        public void TestMethod1()
+        {
+            // empty
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Thread.Sleep(1000);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Thread.Sleep(3000);
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
