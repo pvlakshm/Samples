26febd50cb800fe6ab65406657f1dcaa626d8a73
pvlakshm <pvlakshm@users.noreply.github.com>
2018-10-26 13:55:30 +0530

    Sample for MSTest V2 based data driven tests. (#40)
    * Updated readme.
    
    * Deleting a folder no longer required.
    
    * sync'g (#38)
    
    * Updated readme. (#36)
    
    * Fb1 (#37)
    
    * Updated readme.
    
    * Deleting a folder no longer required.
    
    * Added TimeoutAttribute sample.
    
    * Sample for MSTest V2 based data driven tests.

---
+++ DataDrivenTests/DataDrivenTests/CustomDataDrivenTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Globalization;
+
+namespace DataDrivenTests
+{
+    public class CustomTestDataSource : Attribute, ITestDataSource
+    {
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            return new[] {
+                new object[] {1, 2, 3},
+            };
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            if (data != null)
+            {
+                return string.Format(CultureInfo.CurrentCulture, "{0} ({1})", methodInfo.Name, string.Join(",", data));
+            }
+
+            return null;
+        }
+    }
+
+    [TestClass]
+    public class CustomDataDrivenTest
+    {
+        [DataTestMethod]
+        [CustomTestDataSource]
+        public void TestMethod1(int x, int y, int sum)
+        {
+            var res = x + y;
+            Assert.AreEqual(sum, res);
+        }
+    }
+}
---
+++ DataDrivenTests/DataDrivenTests/DynamicDataDrivenTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDrivenTests
+{
+    [TestClass]
+    public class DynamicTestDataProvider
+    {
+        public static IEnumerable<object[]> GetDataMethod()
+        {
+            yield return new object[] { 1, 2, 3 };
+        }
+
+        public static IEnumerable<object[]> GetDataProperty
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 1, 2, 3 }
+                };
+            }
+        }
+    }
+
+    [TestClass]
+    public class DynamicDataDrivenTest
+    {
+        [DataTestMethod]
+        [DynamicData("GetDataMethod", typeof(DynamicTestDataProvider), DynamicDataSourceType.Method)]
+        public void TestMethod1(int x, int y, int sum)
+        {
+            var res = x + y;
+            Assert.AreEqual(sum, res);
+        }
+
+        [DataTestMethod]
+        [DynamicData("GetDataProperty", typeof(DynamicTestDataProvider), DynamicDataSourceType.Property)]
+        public void TestMethod2(int x, int y, int sum)
+        {
+            var res = x + y;
+            Assert.AreEqual(sum, res);
+        }
+    }
+}
