c3b89ecc64ad6b7a51f05927e2c98fc644f4e03d
Pratap Lakshman <pratapl@microsoft.com>
2018-10-26 13:54:24 +0530

    Sample for MSTest V2 based data driven tests.

---
+++ DataDrivenTests/DataDrivenTests/InlineDataDrivenTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataDrivenTests
+{
+    [TestClass]
+    public class InlineDataDrivenTest
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        public void TestMethod1(int x, int y, int sum)
+        {
+            var res = x + y;
+            Assert.AreEqual(sum, res);
+        }
+    }
+}
---
+++ DataDrivenTests/DataDrivenTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataDrivenTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataDrivenTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("defd8c18-7c99-4b5f-baa7-ce6702ab4175")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
