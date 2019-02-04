da77c8bd10af5478a073e511f9789749f8cfea00
pvlakshm <pvlakshm@users.noreply.github.com>
2019-02-04 12:47:28 +0530

    sync'g (#48)
    * Updated readme.
    
    * Deleting a folder no longer required.
    
    * sync'g (#38)
    
    * Updated readme. (#36)
    
    * Fb1 (#37)
    
    * Updated readme.
    
    * Deleting a folder no longer required.
    
    * Added TimeoutAttribute sample.
    
    * Sample for MSTest V2 based data driven tests.
    
    * Added examples for computed values.

--- DataDrivenTests/DataDrivenTests/InlineDataDrivenTest.cs
+++ DataDrivenTests/DataDrivenTests/InlineDataDrivenTest.cs
@@ -6,8 +6,10 @@ namespace DataDrivenTests
     [TestClass]
     public class InlineDataDrivenTest
     {
+        // The DataRow attribute can be used to provide values inline. These values have to be constants.
         [DataTestMethod]
         [DataRow(1, 2, 3)]
+        [DataRow(1, 2+2, 3+2)]
         public void TestMethod1(int x, int y, int sum)
         {
             var res = x + y;
