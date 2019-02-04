68cb5bd440f08b3ba976e8275fd9265da3e96427
Pratap Lakshman <pratapl@microsoft.com>
2019-02-04 13:16:30 +0530

    Various kinds of flaky tests.

--- FlakyTests/AppTestProject1/UnitTest1.cs
+++ FlakyTests/AppTestProject1/UnitTest1.cs
@@ -6,27 +6,38 @@ namespace AppTestProject1
     [TestClass]
     public class UnitTest1
     {
+        // ------------------------
+        // Flaky SUT business logic
+        // ------------------------
         [TestMethod]
         public void TestIsFlakyCozSUTisFalky()
         {
-            // this test is flaky because the SUT is flaky.
+            // this test is flaky because the SUT business logic is flaky.
             int x = 3;
             int y = 4;
             int sum = 7;
-
             int val = new AppCode.Class1().addFlaky(x, y);
             Assert.AreEqual(sum, val);
         }
 
-        private static int state = new System.Random().Next(2);
+        // ------------------------
+        // Flaky environment
+        // ------------------------
+        private static int state = new System.Random().Next(2); // simulate a flaky environment.
 
         [TestMethod]
         public void TestIsFlakyCozEnvIsFlaky()
         {
             // this test is flaky because the environment is flaky
-            Assert.AreEqual(state, 0);
+            int val = state;
+            Assert.AreEqual(val, 0);
         }
 
+
+        // -----------------------------------------------------
+        // Flaky due to dependence on execution order
+        // This a varition of the case of the flaky environment.
+        // -----------------------------------------------------
         private static bool parentRan = false;
 
         [TestMethod]
